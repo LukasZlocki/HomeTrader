9f1e61fc682ef5da48f029883296d1ab8859596f
lzlocki <lzlocki@poczta.fm>
2020-06-06 20:48:21 +0200

    Add financial models

---
+++ HomeTrader.Data/Models/Finance/FinanceBalance.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HomeTrader.Data.Models.Finance
+{
+    public class FinanceBalance
+    {
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [ForeignKey("CompanyId")]
+        public int CompanyId { get; set; }
+        [Display(Name = "Nazwa")]
+        public string ComapanyName { get; set; }
+        [Display(Name = "rok")]
+        public int RaportYear { get; set; }
+
+        #region Bilans
+        [Display(Name = "Aktywa trwale")]
+        public int AktywaTrwale { get; set; }
+        [Display(Name = "AktywaObrotowe")]
+        public int AktywaObrotowe { get; set; }
+        [Display(Name = "Aktywa Razem")]
+        public int AktywaRazem { get; set; }
+        [Display(Name = "Kapital Wlasny Akcjonariuszy Jednostki Dominujacej")]
+        public int KapitalWlasnyAkcjonariuszyJednostkiDominujacej { get; set; }
+        [Display(Name = "Udzialy Niekontrolujace")]
+        public int UdzialyNiekontrolujace { get; set; }
+        [Display(Name = "Zobowiazania Dlugoterminowe")]
+        public int ZobowiazaniaDlugoterminowe { get; set; }
+        [Display(Name = "Zobowiazania Krotkoterminowe")]
+        public int ZobowiazaniaKrotkoterminowe { get; set; }
+        [Display(Name = "Pasywa Razem")]
+        public int PasywaRazem { get; set; }
+        #endregion
+    }
+}
---
+++ HomeTrader.Data/Models/Finance/FinanceProfitAndLose.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HomeTrader.Data.Models.Finance
+{
+    public class FinanceProfitAndLose
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [ForeignKey("CompanyId")]
+        public int CompanyId { get; set; }
+        [Display(Name = "Nazwa")]
+        public string ComapanyName { get; set; }
+
+        [Display(Name = "Rok")]
+        public int RaportYear { get; set; }
+
+        #region Rachunek zyskow i strat
+        [Display(Name = "Przychody")]
+        public int PrzychodyZeSprzedazy { get; set; }
+        [Display(Name = "Zysk")]
+        public int ZyskZeSprzedazy { get; set; }
+        [Display(Name = "Zysk Operacyjny")]
+        public int ZyskOperacyjny { get; set; }
+        [Display(Name = "Zysk z dzialalnosci gospodarczej")]
+        public int ZyskZDzialalnosciGospodarczej { get; set; }
+        [Display(Name = "Zysk przed opodatkowaniem")]
+        public int ZyskPrzedOpodatkowaniem { get; set; }
+
+        [Display(Name = "Zysk Netto")]
+        public int ZyskNetto { get; set; }
+        public int ZyskNettoAkcjonariuszyJednostkiDominujacej { get; set; }
+
+        [Display(Name = "EBITDA")]
+        public int EBITDA { get; set; }
+        #endregion
+    }
+}
